72153cb24d97be7820a918034d76c9236d94d3b5
Dredur <18683844+Dredur@users.noreply.github.com>
2026-03-08 10:09:50 +0000

    add files for last commit

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ sortDivList();
 
 function sortDivList() {
 
+    // Dieser Code ist nur so lange gültig, bis eine eigene Unterseite für Getränke erstellt wurde.
+    // Danach kann der auskommentierte Code darunter wieder verwendet werden.
+    
+    let recipes = Array.from(recipeListDiv.querySelectorAll(".card"));
+    recipeListDiv.querySelectorAll(".card").forEach(card => card.remove());
+
+    meals = recipes
+    .filter((item1) => {
+        str1 = item1.querySelector("a").textContent;
+        return !str1.includes("Getränk -");
+    })
+    .sort((item1, item2) => {
+        str1 = item1.querySelector("a").textContent;
+        str2 = item2.querySelector("a").textContent;
+        return str1.localeCompare(str2);
+    })
+    .map((item) => recipeListDiv.appendChild(item));
+    
+    drinks = recipes.filter((item1) => {
+        str1 = item1.querySelector("a").textContent;
+        return str1.includes("Getränk -");
+    })
+    .sort((item1, item2) => {
+        str1 = item1.querySelector("a").textContent;
+        str2 = item2.querySelector("a").textContent;
+        return str1.localeCompare(str2);
+    })
+    .map((item) => recipeListDiv.appendChild(item));
+
+    /*Code ohne Getränke rauszufiltern.
+    Ein Entfernen aller Elemente aus dem DOM ist dann ebenfalls nicht erforderlich.
+
     let recipes = Array.from(recipeListDiv.querySelectorAll(".card"));
 
     recipes.sort((item1, item2) => {
@@ -17,6 +49,7 @@ function sortDivList() {
     });
 
     recipes.map((item) => recipeListDiv.appendChild(item));
+    */
 
 }
 
